de8b58f6f69eec2ed8f8116aecff53d35bf72aba
Alex Rekachynskyi <phone.070718@gmail.com>
2025-10-23 12:46:17 +0200

    npm

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -25,7 +25,8 @@ const config: Config = {
   organizationName: 'ConductionNL', // Usually your GitHub org/user name.
   projectName: 'woo-website-template', // Usually your repo name.
 
-  onBrokenLinks: 'throw',
+  onBrokenLinks: 'warn',
+  onBrokenMarkdownLinks: 'warn',
 
   // Even if you don't use internationalization, you can use this field to set
   // useful metadata like html lang. For example, if your site is Chinese, you
